d19d4f0d5a580ece814d00fb48c1524eced7dccd
Owen Green <gungwho@gmail.com>
2019-12-01 19:07:02 +0000

    Add message dump to json

--- include/FluidParameterDump.hpp
+++ include/FluidParameterDump.hpp
@@ -2,6 +2,8 @@
 #include <clients/common/FluidBaseClient.hpp>
 
 #include <clients/common/SharedClientUtils.hpp>
+#include <clients/nrt/DataSetClient.hpp>
+#include <clients/nrt/LabelSetClient.hpp>
 
 #include <nlohmann/json.hpp>
 #include <array>
@@ -144,12 +146,54 @@ std::string getParamType(const LongT&) { return "long"; }
 std::string getParamType(const FloatPairsArrayT&) { return "float";}
 std::string getParamType(const StringT&) { return "symbol";}
 
+std::string getParamType(const std::string&) { return "symbol";}
+
+std::string getParamType(const typename BufferT::type&) { return "buffer"; }
+std::string getParamType(const typename InputBufferT::type&) { return "buffer"; }
+
+template<typename T>
+std::enable_if_t<std::is_integral<T>::value, std::string>
+getParamType(const T&) { return "long"; }
+
+template<typename T>
+std::enable_if_t<std::is_floating_point<T>::value, std::string>
+getParamType(const T&) { return "float"; }
+
+std::string getParamType(const DataSetClientRef&) { return "dataset"; }
+std::string getParamType(const LabelSetClientRef&) { return "labelset"; }
+
+template<typename T>
+std::string getParamType(const FluidTensor<T,1>&) { return "array " + getParamType(T{}); }
+
+
+template<typename...Ts,size_t...Is>
+auto getParamType(const std::tuple<Ts...>&, std::index_sequence<Is...>){
+  std::array<std::string, sizeof...(Ts)> res{{getParamType(std::add_const_t<std::tuple_element_t<Is,std::tuple<Ts...>>>{})...}};
+  return res;
+}
+
+template<typename...Ts>
+auto getParamType(const std::tuple<Ts...>& t)
+{
+  return getParamType(t,std::index_sequence_for<Ts...>());
+}
+
+template<typename T>
+std::string getParamType(const MessageResult<T>&)
+{
+  return getParamType(typename MessageResult<T>::type{});
+}
+
+std::string getParamType(const MessageResult<void>&)
+{
+  return "void";
+}
+
 template<typename ConstraintsTuple, typename ParamsTuple, size_t...Is>
 std::array<json, sizeof...(Is)> constraints(ConstraintsTuple cs, ParamsTuple ps, std::index_sequence<Is...>)
 {
   return {{makeConstraint(std::get<Is>(cs),ps)...}};
 }
-
 
 template<typename Param, typename Tuple, typename AllParams>
 void to_json(json& o, const ParamBundle<Param,Tuple,AllParams>& pb)
@@ -206,27 +250,43 @@ void to_json(json& o, const ParamBundle<FFTParamsT,Tuple,AllParams>& pb)
   o = {p.name, j};
 }
 
+template<typename L, typename Ret, typename T, typename...Args>
+void to_json(json& o, const Message<L,Ret,T,Args...>& m)
+{
+  json j;
+  j["return"] = getParamType(Ret{});
+  j["args"] = getParamType(std::tuple<Args...>{});
+  o = {m.name,j};
+}
 
 template <class T>
 class ParameterDump
 {
 public:
   using  Params = typename ClientWrapper<T>::ParamDescType;
+  using  Messages = typename T::MessageSetType;
 
   static void dump(std::string ofile,std::string prefix)
   {
-    json j = dumpImpl(typename Params::IndexList());
+    json j;
+    j["params"]= dumpParams(typename Params::IndexList());
+    j["messages"] = dumpMessages(typename Messages::IndexList());
     std::ofstream(prefix + ofile + ".json") << j.dump(2) << '\n';
   }
 
   template<size_t...Is>
-  static json dumpImpl(std::index_sequence<Is...>)
+  static json dumpParams(std::index_sequence<Is...>)
   {
     Params& p = ClientWrapper<T>::getParameterDescriptors();
-    json j;
     auto d = p.descriptors();
-    j["params"] = json::array_t{makeParamBundle(std::get<0>(std::get<Is>(d)),std::get<Is>(d),p)...};
-    return j;
+    return json::array_t{makeParamBundle(std::get<0>(std::get<Is>(d)),std::get<Is>(d),p)...};
+  }
+  
+  template<size_t...Is>
+  static json dumpMessages(std::index_sequence<Is...>)
+  {
+    const auto m = T::getMessageDescriptors();
+    return json::array_t{m.template get<Is>()...};
   }
 };
 
