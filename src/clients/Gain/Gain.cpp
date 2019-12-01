4507593af1804d8ea55ef155ef063da83cc71095
Owen Green <gungwho@gmail.com>
2019-12-01 19:08:41 +0000

    Make sure RT objects are referring to their wrapped client versions (otherwise message queries fail to compile)

--- src/clients/Gain/Gain.cpp
+++ src/clients/Gain/Gain.cpp
@@ -4,6 +4,6 @@ int main(int argc, char *argv[]) {
   using namespace fluid::client;
   if (!argc) std::cerr << "Please pass a folder to write to";
   std::cout << "Write JSON to " << argv[1]; 
-  ParameterDump<GainClient>::dump("Gain", argv[1]);
+  ParameterDump<RTGainClient>::dump("Gain", argv[1]);
   return 0;
 }
--- src/clients/Loudness/Loudness.cpp
+++ src/clients/Loudness/Loudness.cpp
@@ -4,6 +4,6 @@ int main(int argc, char *argv[]) {
   using namespace fluid::client;
   if (!argc) std::cerr << "Please pass a folder to write to";
   std::cout << "Write JSON to " << argv[1];
-  ParameterDump<LoudnessClient>::dump("Loudness", argv[1]);
+  ParameterDump<RTLoudnessClient>::dump("Loudness", argv[1]);
   return 0;
 }
