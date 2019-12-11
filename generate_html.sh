9e419a5a7511e867444f8f058d94088a61ec72fc
Owen Green <gungwho@gmail.com>
2019-12-11 00:35:10 +0000

    PD / CLI doc gen fix part deux

--- generate_html.sh
+++ generate_html.sh
@@ -4,6 +4,6 @@ for a in maxref/fluid.*.xml
 do
   destfile=${a#maxref/}
   destfile=${destfile%.maxref.xml}
-  xsltproc --stringparam compliant 1 xslt/_c74_ref.xsl $a >  html/${destfile}.html
+  xsltproc --stringparam compliant 1 $1/xslt/_c74_ref.xsl $a >  html/${destfile}.html
 done
-cp xslt/_c74_common.css html/
+cp $1/xslt/_c74_common.css html/
