f2a14d3721522ebaa4e719cfa05322c204ae92f8
Owen Green <gungwho@gmail.com>
2020-01-15 17:40:36 +0000

    Remove XSLT and old HTML gen stuff

--- generate_html.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mkdir html
-for a in maxref/fluid.*.xml
-do
-  destfile=${a#maxref/}
-  destfile=${destfile%.maxref.xml}
-  xsltproc --stringparam compliant 1 $1/xslt/_c74_ref.xsl $a >  html/${destfile}.html
-done
-cp $1/xslt/_c74_common.css html/
