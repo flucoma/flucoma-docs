38b63f029ff6158e4428bd38c81d17bcf59c4d58
Owen Green <gungwho@gmail.com>
2020-06-19 14:39:28 +0100

    Remove KNN, add KNNClassifier + KNNRegressor

--- src/clients/KNNClassifier/KNNClassifier.cpp
+++ src/clients/KNNClassifier/KNNClassifier.cpp
@@ -1,9 +1,9 @@
-#include <clients/nrt/kNNClient.hpp>
+#include <clients/nrt/KNNClassifierClient.hpp>
 #include <FluidParameterDump.hpp>
 int main(int argc, char *argv[]) {
   using namespace fluid::client;
   if (!argc) std::cerr << "Please pass a folder to write to";
   std::cout << "Write JSON to " << argv[1];
-  ParameterDump<NRTThreadedKNNClient>::dump("KNN", argv[1]);
+  ParameterDump<NRTThreadedKNNClassifierClient>::dump("KNNClassifier", argv[1]);
   return 0;
 }
