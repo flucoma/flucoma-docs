8e64fc66b42c5cf360226e1a4422233b6c82ac48
Owen Green <gungwho@gmail.com>
2020-07-08 00:34:00 +0100

    Add new objects

---
+++ src/clients/DataSetQuery/DataSetQuery.cpp
@@ -0,0 +1,9 @@
+#include <clients/nrt/DataSetQueryClient.hpp>
+#include <FluidParameterDump.hpp>
+int main(int argc, char *argv[]) {
+  using namespace fluid::client;
+  if (!argc) std::cerr << "Please pass a folder to write to";
+  std::cout << "Write JSON to " << argv[1];
+  ParameterDump<NRTThreadedDataSetQueryClient>::dump("DataSetQuery", argv[1]);
+  return 0;
+}
