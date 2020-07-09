2fdbf738c2b0c9b0c37822806678a052df4f0ea9
Pierre Alexandre Tremblay <info@pierrealexandretremblay.com>
2020-07-09 10:26:19 +0100

    error in destination name for standardize

--- src/clients/Standardize/Standardize.cpp
+++ src/clients/Standardize/Standardize.cpp
@@ -4,6 +4,6 @@ int main(int argc, char *argv[]) {
   using namespace fluid::client;
   if (!argc) std::cerr << "Please pass a folder to write to";
   std::cout << "Write JSON to " << argv[1];
-  ParameterDump<NRTThreadedStandardizeClient>::dump("Normalize", argv[1]);
+  ParameterDump<NRTThreadedStandardizeClient>::dump("Standardize", argv[1]);
   return 0;
 }
