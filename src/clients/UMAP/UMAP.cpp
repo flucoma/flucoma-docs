705c19b2f9e6e789ba50deedf37bd5d3249c4a19
Pierre Alexandre Tremblay <info@pierrealexandretremblay.com>
2020-10-09 11:30:02 +0100

    UMAP ref added

---
+++ src/clients/UMAP/UMAP.cpp
@@ -0,0 +1,9 @@
+#include <clients/nrt/UMAPClient.hpp>
+#include <FluidParameterDump.hpp>
+int main(int argc, char *argv[]) {
+  using namespace fluid::client;
+  if (!argc) std::cerr << "Please pass a folder to write to";
+  std::cout << "Write JSON to " << argv[1];
+  ParameterDump<NRTThreadedUMAPClient>::dump("UMAP", argv[1]);
+  return 0;
+}
