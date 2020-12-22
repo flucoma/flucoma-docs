0885f7cbd84c43c6366b63fbcb16cbd550744546
Pierre Alexandre Tremblay <info@pierrealexandretremblay.com>
2020-12-22 09:35:27 +0000

    sorted the reference in UMAP

--- src/clients/UMAP/UMAP.cpp
+++ src/clients/UMAP/UMAP.cpp
@@ -4,6 +4,6 @@ int main(int argc, char *argv[]) {
   using namespace fluid::client;
   if (!argc) std::cerr << "Please pass a folder to write to";
   std::cout << "Write JSON to " << argv[1];
-  ParameterDump<NRTThreadedUMAPClient>::dump("UMAP", argv[1]);
+  ParameterDump<RTUMAPClient>::dump("UMAP", argv[1]);
   return 0;
 }
