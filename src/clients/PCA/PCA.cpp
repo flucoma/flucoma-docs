433c2581c84608d7d8aa2f7201d88d83099a267c
Owen Green <gungwho@gmail.com>
2021-09-20 11:27:39 +0100

    Named PCA, MLPs, UMAP

--- src/clients/PCA/PCA.cpp
+++ src/clients/PCA/PCA.cpp
@@ -4,6 +4,6 @@ int main(int argc, char *argv[]) {
   using namespace fluid::client;
   if (!argc) std::cerr << "Please pass a folder to write to";
   std::cout << "Write JSON to " << argv[1];
-  ParameterDump<RTPCAClient>::dump("PCA", argv[1]);
+  ParameterDump<NRTThreadedPCAClient>::dump("PCA", argv[1]);
   return 0;
 }
