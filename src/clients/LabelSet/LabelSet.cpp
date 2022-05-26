3291b01e812bd68f5fd6d6f447744c02e3cb7bb3
Owen Green <gungwho@gmail.com>
2022-05-26 09:57:27 +0100

    Enhance/generate stubs (#119)
    * CMake: Generate cpp stubs, smooth out API for dependent projects
    
    * Remove old stubs and scripts
    
    * CMake: Ensure correct MSVC runtime by default
    
    * Correct variable setting logic in macro

--- src/clients/LabelSet/LabelSet.cpp
+++
@@ -1,9 +0,0 @@
-#include <clients/nrt/LabelSetClient.hpp>
-#include <FluidParameterDump.hpp>
-int main(int argc, char *argv[]) {
-  using namespace fluid::client;
-  if (!argc) std::cerr << "Please pass a folder to write to";
-  std::cout << "Write JSON to " << argv[1];
-  ParameterDump<NRTThreadedLabelSetClient>::dump("LabelSet", argv[1]);
-  return 0;
-}
