0cc3faddf6e6db1f295c9f018846b22678d16677
James Bradbury <github@jamesbradbury.net>
2022-05-30 10:23:21 +0100

    use a regex instead of object map

--- flucoma/doc/learn.py
+++ flucoma/doc/learn.py
@@ -1,22 +1,25 @@
 def derive_learn_link(object_name):
-    url_map = {
-        'bufampfeature' : 'ampfeature',
-        'bufnoveltyfeature' : 'noveltyfeature',
-        'bufonsetfeature' : 'onsetfeature',
-        'bufspectralshape' : 'spectralshape',
-        'bufchroma' : 'chroma',
-        'bufloudness' : 'loudness',
-        'bufmelbands' : 'melbands',
-        'bufmfcc' : 'mfcc',
-        'bufpitch' : 'pitch',
-        'bufhpss' : 'hpss',
-        'bufsines' : 'sines',
-        'buftransients' : 'transients',
-        'bufampgate' : 'ampgate',
-        'bufampslice' : 'ampslice',
-        'bufnoveltyslice' : 'noveltyslice',
-        'bufonsetslice' : 'onsetslice',
-        'buftransientslice' : 'transientslice',
-        'bufaudiotransport' : 'audiotransport'
-    }
-    return url_map.get(object_name.lower()) or object_name.lower()
+    m = re.search('(?:Buf)(?!NMF|Compose)(.+)', object_name)
+    return m.group(1).lower() or object_name.lower()
+
+    # url_map = {
+    #     'bufampfeature' : 'ampfeature',
+    #     'bufnoveltyfeature' : 'noveltyfeature',
+    #     'bufonsetfeature' : 'onsetfeature',
+    #     'bufspectralshape' : 'spectralshape',
+    #     'bufchroma' : 'chroma',
+    #     'bufloudness' : 'loudness',
+    #     'bufmelbands' : 'melbands',
+    #     'bufmfcc' : 'mfcc',
+    #     'bufpitch' : 'pitch',
+    #     'bufhpss' : 'hpss',
+    #     'bufsines' : 'sines',
+    #     'buftransients' : 'transients',
+    #     'bufampgate' : 'ampgate',
+    #     'bufampslice' : 'ampslice',
+    #     'bufnoveltyslice' : 'noveltyslice',
+    #     'bufonsetslice' : 'onsetslice',
+    #     'buftransientslice' : 'transientslice',
+    #     'bufaudiotransport' : 'audiotransport'
+    # }
+    # return url_map.get(object_name.lower()) or object_name.lower()
